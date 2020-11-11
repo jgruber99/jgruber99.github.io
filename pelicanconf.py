3a5d360c4b2c0d376634c0d88bbe59a14a6cbfe8
Julius <jgruber@student.ethz.ch>
2020-11-11 14:14:06 +0100

    adding lots of stuff

--- pelicanconf.py
+++ pelicanconf.py
@@ -1,35 +1,66 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*- #
+AUTHOR = 'Alexis Métaireau'
+SITENAME = "Alexis' log"
+SITESUBTITLE = 'A personal blog.'
+SITEURL = 'http://blog.notmyidea.org'
+TIMEZONE = "Europe/Paris"
 
-AUTHOR = 'Julius Gruber'
-SITENAME = 'Julius Website'
-SITEURL = ''
+# can be useful in development, but set to False when you're ready to publish
+RELATIVE_URLS = True
 
+GITHUB_URL = 'http://github.com/ametaireau/'
+DISQUS_SITENAME = "blog-notmyidea"
+REVERSE_CATEGORY_ORDER = True
+LOCALE = "C"
+DEFAULT_PAGINATION = 4
+DEFAULT_DATE = (2012, 3, 2, 14, 1, 1)
+
+FEED_ALL_RSS = 'feeds/all.rss.xml'
+CATEGORY_FEED_RSS = 'feeds/{slug}.rss.xml'
+
+LINKS = (('Biologeek', 'http://biologeek.org'),
+         ('Filyb', "http://filyb.info/"),
+         ('Libert-fr', "http://www.libert-fr.com"),
+         ('N1k0', "http://prendreuncafe.com/blog/"),
+         ('Tarek Ziadé', "http://ziade.org/blog"),
+         ('Zubin Mithra', "http://zubin71.wordpress.com/"),)
+
+SOCIAL = (('twitter', 'http://twitter.com/ametaireau'),
+          ('lastfm', 'http://lastfm.com/user/akounet'),
+          ('github', 'http://github.com/ametaireau'),)
+
+# global metadata to all the contents
+DEFAULT_METADATA = {'yeah': 'it is'}
+
+# path-specific metadata
+EXTRA_PATH_METADATA = {
+    'extra/robots.txt': {'path': 'robots.txt'},
+    }
+
+# static paths will be copied without parsing their contents
+STATIC_PATHS = [
+    'images',
+    'extra/robots.txt',
+    ]
+
+# custom page generated with a jinja2 template
+TEMPLATE_PAGES = {'pages/jinja2_template.html': 'jinja2_template.html'}
+
+# there is no other HTML content
+READERS = {'html': None}
+
+# code blocks with line numbers
+PYGMENTS_RST_OPTIONS = {'linenos': 'table'}
+
+# foobar will not be used, because it's not in caps. All configuration keys
+# have to be in caps
+foobar = "barbaz"
+
+THEME = 'themes/bootstrap2'
+OUTPUT_PATH = 'output'
 PATH = 'content'
 
-TIMEZONE = 'Europe/Paris'
+# Custom Home page
+DIRECT_TEMPLATES = (('index', 'blog', 'tags', 'categories', 'archives'))
+PAGINATED_DIRECT_TEMPLATES = (('blog',))
+TEMPLATE_PAGES = {'home.html': 'index.html',}
 
-DEFAULT_LANG = 'en'
-
-THEME = '.'
-# Feed generation is usually not desired when developing
-FEED_ALL_ATOM = None
-CATEGORY_FEED_ATOM = None
-TRANSLATION_FEED_ATOM = None
-AUTHOR_FEED_ATOM = None
-AUTHOR_FEED_RSS = None
-
-# Blogroll
-LINKS = (('Pelican', 'https://getpelican.com/'),
-         ('Python.org', 'https://www.python.org/'),
-         ('ETHZ', 'https://ethz.ch/de.html'),)
-            
-# Social widget
-SOCIAL = (('You can add links in your config file', '#'),
-        ('My Linkedin', 'https://www.linkedin.com/in/julius-gruber-4a1541157'),
-          )
-
-DEFAULT_PAGINATION = 10
-
-# Uncomment following line if you want document-relative URLs when developing
-#RELATIVE_URLS = True
